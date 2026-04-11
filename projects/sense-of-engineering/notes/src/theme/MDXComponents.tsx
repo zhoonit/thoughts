a5ab094b7252b9772c3ae3295627cdc538e25fac
Jihoon Lee <zhoonit@gmail.com>
2026-04-11 22:57:05 +0900

    feat: wire CodePlayground into notes with interactive ch01 example

--- projects/sense-of-engineering/notes/src/theme/MDXComponents.tsx
+++ projects/sense-of-engineering/notes/src/theme/MDXComponents.tsx
@@ -1,9 +1,11 @@
 import MDXComponents from "@theme-original/MDXComponents";
 import { CodeBlock, Callout, Figure } from "@lectures/shared";
+import { CodePlayground } from "@lectures/shared/playground";
 
 export default {
   ...MDXComponents,
   CodeBlock,
   Callout,
   Figure,
+  CodePlayground,
 };
