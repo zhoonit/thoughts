2f83192cda2736784d06179abe4762b33de3d163
Jihoon Lee <zhoonit@gmail.com>
2026-04-11 22:32:10 +0900

    feat: wire shared components into notes + add ch01 sample

---
+++ projects/sense-of-engineering/notes/src/theme/MDXComponents.tsx
@@ -0,0 +1,9 @@
+import MDXComponents from "@theme-original/MDXComponents";
+import { CodeBlock, Callout, Figure } from "@lectures/shared";
+
+export default {
+  ...MDXComponents,
+  CodeBlock,
+  Callout,
+  Figure,
+};
